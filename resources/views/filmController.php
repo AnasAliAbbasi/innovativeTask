269e56fff66de2ae142b16d073c080d66eeab98f
Anas Ali Abbasi <72650878+AnasAliAbbasi@users.noreply.github.com>
2023-07-21 16:50:31 +0500

    Update filmController.php

--- resources/views/filmController.php
+++ resources/views/filmController.php
@@ -91,3 +91,46 @@ class filmController extends Controller
         
     }
 }
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FilmsListingTest extends TestCase
+{
+
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response = $this->get('film-create');
+
+
+        $response->assertStatus(200);
+        $filmData = [
+            "name" => "fiml_1",
+            "description"=>  "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia\n",
+            "release_date"=> "2023-07-19",
+            "rating"=> 5,
+            "ticket_price"=> 500,
+            "country"=> "pakistan",
+            "image"=> "photo.jpeg"
+        ];
+
+        $this->post('insert/movie', $filmData);
+        $this->assertDatabaseHas('films', $filmData);
+        $response->assertStatus(200);
+    }
+}
+
+
+
+
+
+
+
+
